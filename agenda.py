d453b4707d1f2099482969991562060c7ab20f00
Your Name <you@example.com>
2023-12-13 19:24:10 -0300

    1

---
+++ agenda.py
@@ -0,0 +1,31 @@
+def menu():
+   opcao=input('''
+====================================
+            Agenda Pessoal
+Menu:
+[1]Cadastrar contato
+[2]Deletar contato
+[3]Buscar contato pelo id
+[4]Tarefas
+[5]Compromissos
+[6]Deletar tarefas
+[7]Deletar Compromissos
+=====================================
+    Escolha uma opção acima.''')
+   if opcao=="1":
+      cadastrarContato()
+   elif opcao=="2":
+      deletarContato()
+   else:
+      buscarContatoPeloId()
+def cadastrarContato():
+   idContato=input("Escolha o id do contato")
+   return print(f'Cadastrar um contato: ') 
+def deletarContato():
+   print("Deletar contato:")
+def buscarContatoPeloId():
+   print("Buscar contato pelo id:")
+  
+def main():
+   menu()
+main()
