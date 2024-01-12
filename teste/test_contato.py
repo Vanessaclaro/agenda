9b43fdb455d757c9e4ba4e6d907ba86ebdf4e1c4
Your Name <you@example.com>
2024-01-12 19:24:22 -0300

    test

--- teste/test_contato.py
+++ teste/test_contato.py
@@ -1,5 +1,12 @@
+import pytest
 from agenda import cadastrarContato
-def test_cadastrarContato():
+def test_cadastrarContato(mocker, capsys):
+    
+ mocker.patch('builtins.input', side_effect=['João', 'joao@email.com', '123456789', 'Rua A'])
+    
+cadastrarContato()
 
-from agenda import tarefas:
-def test_tarefas():
+captured = capsys.readouterr()
+assert('Contato gravado com sucesso')
+
+
